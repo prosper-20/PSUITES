125cd9e856a4a721569c486643b99e98b0546b91
prosper-20 <edwardprosper001@gmail.com>
2022-03-29 16:43:08 +0100

    updates

--- PROJECT/hotel/views.py
+++ PROJECT/hotel/views.py
@@ -1,3 +1,14 @@
+from typing import List
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Room, Booking
 
 # Create your views here.
+
+class RoomList(ListView):
+    model = Room
+
+
+class BookingList(ListView):
+    model = Booking
+    
