4996f550e033690e107d0f4144f38f481bbd9d6c
prosper-20 <edwardprosper001@gmail.com>
2022-03-29 16:03:48 +0100

    updates

--- PROJECT/hotel/booking_functions/availability.py
+++ PROJECT/hotel/booking_functions/availability.py
@@ -2,3 +2,10 @@ import datetime
 from hotel.models import Book, Booking
 
 def check_availability(room, check_in, check_out):
+    avail_list = []
+    booking_list = Booking.objects.filter(room=room)
+    for booking in booking_list:
+        if booking.check_in > check_out or booking.check_out < check_in:
+            avail_list.append(True)
+        else:
+            avail_list.append(False)
