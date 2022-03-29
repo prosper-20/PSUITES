e4fd070a572f62032618ca30bb15ce3bdef35df3
prosper-20 <edwardprosper001@gmail.com>
2022-03-29 16:34:03 +0100

    updates

--- PROJECT/hotel/models.py
+++ PROJECT/hotel/models.py
@@ -27,4 +27,4 @@ class Booking(models.Model):
     check_out = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.user} has boked {self.room} from {self.check_in} to {self.check_out}"
+        return f"{self.user} has booked {self.room} from {self.check_in} to {self.check_out}"
