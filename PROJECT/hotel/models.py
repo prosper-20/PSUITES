8bdfcc6357244aaf9bea6e6e8676982daaca8179
prosper-20 <edwardprosper001@gmail.com>
2022-03-29 15:35:27 +0100

    updates

--- PROJECT/hotel/models.py
+++ PROJECT/hotel/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    ROOM_CATEGORIES=(
+        ('EXE', 'EXECUTIVE'),
+        ('DEL', 'DELUXE'),
+        ('PRE', 'PREMIUM'),
+        ('STD', 'STANDARD'),
+        ('PNT', 'PENTHOUSE')
+    )
+    number = models.IntegerField()
+    category = models.CharField(max_length=3, choices=ROOM_CATEGORIES)
+    beds = models.IntegerField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.number} - {self.category}"
