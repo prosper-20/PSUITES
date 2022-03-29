28763b6829be29db74cabdbf165151aaf2600c47
prosper-20 <edwardprosper001@gmail.com>
2022-03-29 16:49:09 +0100

    updates

--- PROJECT/hotel/urls.py
+++ PROJECT/hotel/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import RoomList, BookingList
+
+app_name = "hotel"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("room_list/", RoomList.as_view(), name='RoomList'),
+    path("booking-list/", BookingList.as_view(), name="Booking-List"),
 ]
