63bbaa4b83e0be59958d66e9e0c08824745e3eb1
prosper-20 <edwardprosper001@gmail.com>
2022-03-30 22:44:05 +0100

    updates

--- PROJECT/PROJECT/settings.py
+++ PROJECT/PROJECT/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- PROJECT/PROJECT/urls.py
+++ PROJECT/PROJECT/urls.py
@@ -20,5 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("hotel.urls"))
+    path("", include("hotel.urls")),
+    path('accounts/', include('allauth.urls')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
