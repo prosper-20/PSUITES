b02dc70c4d079d7e597d1c6a9cd54b2d53006cb6
prosper-20 <edwardprosper001@gmail.com>
2022-03-30 02:38:46 +0100

    updates

--- PROJECT/hotel/urls.py
+++ PROJECT/hotel/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RoomList, BookingList, BookingView
+from .views import RoomListView, BookingList, BookingView, RoomDetailView
 
 
 urlpatterns = [
     path("room-list/", RoomList.as_view(), name='RoomList'),
     path("booking-list/", BookingList.as_view(), name="Booking-List"),
-    path("book/", BookingView.as_view(), name="booking_view" )
+    path("book/", BookingView.as_view(), name="booking_view" ),
+    path("room/<category>/", RoomDetailView.as_view(), name='RoomDetailView')
 ]
--- PROJECT/hotel/views.py
+++ PROJECT/hotel/views.py
@@ -1,18 +1,49 @@
 from typing import List
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, FormView, View
 
 from hotel.forms import AvailabilityForm 
 from .models import Room, Booking
 from hotel.booking_functions.availability import check_availability
 # Create your views here.
 
-class RoomList(ListView):
+class RoomListView(ListView):
     model = Room
 
 
 class BookingList(ListView):
     model = Booking
+
+
+class RoomDetailView(View):
+    def get(self, request, *args, **kwargs):
+            room_category = self.kwargs.get('category', None)
+            room_list = Room.objects.filter(category=category)
+            room = room_category[0]
+            return render()
+
+        
+
+    def post(self, request, *args, **kwargs):
+         room_list = Room.objects.filter(category=category)
+        available_rooms = []
+        for room in room_list:
+            if check_availability(room, data['check_in'], data['check_out']):
+                available_rooms.append(room)
+
+        if len(available_rooms) > 0:
+            room = available_rooms[0]
+            booking = Booking.objects.create(
+                user=self.request.user,
+                room=room,
+                check_in=data['check_in'],
+                check_out=data['check_out']
+            )
+            booking.save()
+            return HttpResponse(booking)
+        else:
+            return HttpResponse("This category of rooms are fully booked!")
+
 
 
 class BookingView(FormView):
