a1e585473214fc9482ed2ecd144c47f1c66ebaee
prosper-20 <edwardprosper001@gmail.com>
2022-04-02 22:10:52 +0100

    updates

--- PROJECT/hotel/booking_functions/get_room_list.py
+++ PROJECT/hotel/booking_functions/get_room_list.py
@@ -1,11 +1,16 @@
 from django.urls import reverse
-from Core.models import Book
+from hotel.models import Room
 
 def get_room_list():
-    room_list = []
+    room = Room.objects.all()[0]
     room_categories = dict(room.ROOM_CATEGORIES)
+
+    room_cat_url__list = []
     
     for category in room_categories:
         room_category = room_categories.get(category)
         room_url = reverse('RoomDetailView', kwargs={'category': room_category})
-        room_list.append((room_category, room_url))
+        room_cat_url__list.append((room_category, room_url))
+
+    
+    return room_cat_url__list
